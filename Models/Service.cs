69c103d761117a411931437e53b57e262ae910d6
Mohsin Khan <muhammadmohsinkhan.aptech@gmail.com>
2024-05-28 19:23:35 +0500

    service model update

--- Models/Service.cs
+++ Models/Service.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OnlineMobileRecharge.Models
 {
@@ -7,6 +9,14 @@ namespace OnlineMobileRecharge.Models
         [Key]
         public int Service_Id { get; set; }
         [Required]
-        public int MyProperty { get; set; }
+        [ForeignKey("IdentityUser")]
+        public required string User_Id { get; set; }
+        public required IdentityUser IdentityUser { get; set; }
+        [Required]
+        public bool Do_Not_Disturb { get; set; }
+        [Required]
+        [ForeignKey("Caller_Tune")]
+        public int Caller_Tune_Id { get; set; }
+        public Caller_Tune Caller_Tune { get; set; }
     }
 }
