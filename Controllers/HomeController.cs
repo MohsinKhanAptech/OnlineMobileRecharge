152455cacab1696502a385b93f06b1d2ebc11d85
Mohsin Khan <muhammadmohsinkhan.aptech@gmail.com>
2024-05-29 00:52:53 +0500

    updated home controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using OnlineMobileRecharge.Data;
 using OnlineMobileRecharge.Models;
 using System.Diagnostics;
 
@@ -7,10 +8,12 @@ namespace OnlineMobileRecharge.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
