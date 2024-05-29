73519530063aaa9b2921d2c76a293fcb5d52bcc7
Mohsin Khan <muhammadmohsinkhan.aptech@gmail.com>
2024-05-29 22:49:04 +0500

    added model for contact us page

---
+++ Models/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineMobileRecharge.Models
+{
+    public class Contact
+    {
+        [Key]
+        public int Contact_Id { get; set; }
+        [Required]
+        public required string Contact_Name { get; set; }
+        [Required, EmailAddress]
+        public required string Contact_Email { get; set; }
+        [Required, Phone]
+        public required string Contact_Phone { get; set; }
+        [Required]
+        public required string Contact_Intrest { get; set; }
+        [Required]
+        public required string Contact_Message { get; set; }
+    }
+}
