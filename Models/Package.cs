b2312b7aac9dcd158cd3a98b8ee5d0bbd79f874b
Mohsin Khan <muhammadmohsinkhan.aptech@gmail.com>
2024-05-30 17:35:55 +0500

    changes to package model

--- Models/Package.cs
+++ Models/Package.cs
@@ -1,29 +1,48 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace OnlineMobileRecharge.Models
 {
     public class Package
     {
         [Key]
+        [DisplayName("Id")]
         public int Package_Id { get; set; }
         [Required]
+        [DisplayName("Name")]
         public required string Package_Name { get; set; }
         [Required]
+        [DisplayName("Description")]
         public required string Package_Description { get; set; }
         [Required]
+        [DisplayName("Offnet Mins")]
+        [Range(0,int.MaxValue)]
         public int Package_Off_Net_Mins { get; set; }
         [Required]
+        [DisplayName("Onnet Mins")]
+        [Range(0,int.MaxValue)]
         public int Package_On_Net_Mins { get; set; }
         [Required]
+        [DisplayName("Data")]
+        [Range(0,int.MaxValue)]
         public int Package_Data { get; set; }
         [Required]
-        public int Package_Sms { get; set; }
+        [DisplayName("SMS")]
+        [Range(0,int.MaxValue)]
+        public int Package_SMS { get; set; }
         [Required]
-        public double Package_Price { get; set; }
+        [DisplayName("Duration")]
+        [Range(0,int.MaxValue)]
+        public int Package_Duration { get; set; }
         [Required]
-        public PackageType Package_Type { get; set; }
+        [DisplayName("Price")]
+        public int Package_Price { get; set; }
+        [Required]
+        [EnumDataType(typeof(EnumPackageType))]
+        [DisplayName("Type")]
+        public EnumPackageType Package_Type { get; set; }
     }
-    public enum PackageType
+    public enum EnumPackageType
     {
         Prepaid,
         Postpaid
